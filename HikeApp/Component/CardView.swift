e49840a02ead199e24936bfcca4d019ebc8d4f66
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-10 17:51:17 -0300

    Componentization

---
+++ HikeApp/Component/CardView.swift
@@ -0,0 +1,43 @@
+//
+//  CardView.swift
+//  HikeApp
+//
+//  Created by Kim Ruan on 10/09/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: - CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
