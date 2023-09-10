b2029b4f7e946af887e1df3f4f4bda1be3b289ff
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-10 16:29:31 -0300

    Hello World!

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Kim Ruan on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
